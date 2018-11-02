fcfac0758df19d1302ccf1300e581fe0c1d890a0
LineG <ghanemline@gmail.com>
2018-11-02 19:06:05 -0400

    mapper refactoring yo current UML  #47

--- class/IdentityMap.js
+++ class/IdentityMap.js
@@ -1,54 +1,28 @@
-class IdentityMap {
+class IdentityMap
+{
 
-	constructor(){
-		this.users = new Array();
-		this.items = new Array();
-	}
-
-	usersAlreadyFetched(){
-		if (this.users.length == 0)
-			return false
-		else return true;
-	}
+    constructor()
+    {
+        var ItemList = new ArrayList;
+        
+    }
 
 
-// checks if a specific user is already fetched
-	/*
-	userAlreadyFetched(user){
-		for (let i=0; i<users.length; i++) {
-			if (users[i]==user) {return true;}
-		}
-		return false;
-	}
-	*/
-// checks if a specific item is already fetched
-	itemAlreadyFetched(item){
-		for (let i=0; i<items.length; i++) {
-			if (items[i]==item) {return true;}
-		}
-		return false;
-	}
+    putData( item)
+    {
+       ItemList.add(item)
+    }
 
-	fetchUsers() {
-		return this.users;
-	}
- 
-// fetch all items
-	 fetchItems() {
-		return this.items;
-	}
+    
 
-	addUsers(users) {
-		for (let i=0; i<users.length; i++) {
-			this.users.push(users[i]);
-		}
-	}
+    find(item)
+    {
+        return ItemList.contains(item)
+    }
 
-// add items
-	addItems(items) {
-		for (let i=0; i<items.length; i++) {
-			this.items.push(items[i]);
-		}
-	}
+    empty()
+    {
+        ItemList.clear();
+    }
+
 }
-module.exports = IdentityMap;
---
+++ class/IdentityMapOLD.js
@@ -0,0 +1,56 @@
+class IdentityMapOLD {
+
+
+	
+	constructor(){
+		this.users = new Array();
+		this.items = new Array();
+	}
+
+	usersAlreadyFetched(){
+		if (this.users.length == 0)
+			return false
+		else return true;
+	}
+
+
+// checks if a specific user is already fetched
+	/*
+	userAlreadyFetched(user){
+		for (let i=0; i<users.length; i++) {
+			if (users[i]==user) {return true;}
+		}
+		return false;
+	}
+	*/
+// checks if a specific item is already fetched
+	itemAlreadyFetched(item){
+		for (let i=0; i<items.length; i++) {
+			if (items[i]==item) {return true;}
+		}
+		return false;
+	}
+
+	fetchUsers() {
+		return this.users;
+	}
+ 
+// fetch all items
+	 fetchItems() {
+		return this.items;
+	}
+
+	addUsers(users) {
+		for (let i=0; i<users.length; i++) {
+			this.users.push(users[i]);
+		}
+	}
+
+// add items
+	addItems(items) {
+		for (let i=0; i<items.length; i++) {
+			this.items.push(items[i]);
+		}
+	}
+}
+module.exports = IdentityMap;
