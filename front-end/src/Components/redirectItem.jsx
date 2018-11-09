7cfcac84cddc84d2bbb2178cadb200436aabc776
Marc-Henry Noon <marc.noon@gmail.com>
2018-11-08 21:53:33 -0500

    #81

--- front-end/src/Components/redirectItem.jsx
+++ front-end/src/Components/redirectItem.jsx
@@ -23,9 +23,9 @@ class RedirectItem extends Component {
     if (this.props.profile.Type === "Book"){
         return(<BookProfile bookProfile= {this.props.profile} closeProfile= {this.closeProfile}/>)
     }
-    else if (this.props.profile.Type === "Music"){return(<div>this is music</div>)} 
-    else if (this.props.profile.Type === "Magazine") {return(<div>this is magazine</div>)}
-    else  {return(<div>this is movie</div>)}
+    else if (this.props.profile.Type === "Music"){return(<MusicProfile musicProfile= {this.props.profile} closeProfile= {this.closeProfile}/>)} 
+    else if (this.props.profile.Type === "Magazine") {return(<MagazineProfile magazineProfile= {this.props.profile} closeProfile= {this.closeProfile}/>)}
+    else  {return(<MovieProfile movieProfile= {this.props.profile} closeProfile= {this.closeProfile}/>)}
         }   
 }
 function mapStateToProps(state){
