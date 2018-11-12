3a1735998d2371be2e6f2b5b446ab4aeef0e5fee
eglen <eglen@live.com>
2018-11-12 00:03:16 -0500

    #129 Admin can view Loaned items and their description for all Users

--- class/TDG/LoanTDG.js
+++ class/TDG/LoanTDG.js
@@ -104,15 +104,68 @@ class LoanTDG {
 
   viewAllLoans(callback) {
     let msg = {}; //array that will keep all the loan data
-    let sqlBook = 'SELECT * FROM BookDesc INNER JOIN (SELECT * FROM BookPh INNER JOIN Loan ON BookPh.id = Loan.itemId) AS T ON BookDesc.idDesc = T.idDesc';
     
+    //get all the loaned books
+    let sqlBook = 'SELECT * FROM BookDesc INNER JOIN (SELECT * FROM BookPh INNER JOIN Loan ON BookPh.id = Loan.itemId) AS T ON BookDesc.idDesc = T.idDesc';
+    let sqlMagazine = 'SELECT * FROM MagazineDesc INNER JOIN (SELECT * FROM MagazinePh INNER JOIN Loan ON MagazinePh.id = Loan.itemId) AS T ON MagazineDesc.idDesc = T.idDesc'; 
+    let sqlMusic = 'SELECT * FROM MusicDesc INNER JOIN (SELECT * FROM MusicPh INNER JOIN Loan ON MusicPh.id = Loan.itemId) AS T ON MusicDesc.idDesc = T.idDesc';
+    let sqlMovies = 'SELECT * FROM MovieDesc INNER JOIN (SELECT * FROM MoviePh INNER JOIN Loan ON MoviePh.id = Loan.itemId) AS T ON MovieDesc.idDesc = T.idDesc';
+
     this.runQuery(function(conn, completedQuery) {
       conn.query(sqlBook, (err, rows, fields) => {
         if (!err) {
           msg.success = "true";
           msg.message = "no message";
-          msg.data = rows;
-          callback(msg);
+          //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+          conn.query(sqlMagazine, (err, rows, fields) => {
+            if (!err) {
+              msg.success = "true";
+              msg.message = "no message";
+                  //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+                                  conn.query(sqlMusic, (err, rows, fields) => {
+                                    if (!err) {
+                                      msg.success = "true";
+                                      msg.message = "no message";
+                                      //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+                                                      conn.query(sqlMovies, (err, rows, fields) => {
+                                                        if (!err) {
+                                                          msg.success = "true";
+                                                          msg.message = "no message";
+                                                          msg.movies = rows;
+                                                          callback(msg);
+                                                        } else {
+                                                          console.log(err);
+                                                          msg.success = "false";
+                                                          msg.message = err.sqlMessage;
+                                                          callback(msg);
+                                                        }
+                                                        completedQuery("View All Loan Records");
+                                                      });  
+                                      //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+                                      msg.music = rows;
+                                      //callback(msg);
+                                    } else {
+                                      console.log(err);
+                                      msg.success = "false";
+                                      msg.message = err.sqlMessage;
+                                      callback(msg);
+                                    }
+                                    completedQuery("View All Loan Records");
+                                  });
+                  //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+              msg.magazines = rows;
+              //callback(msg);
+            } else {
+              console.log(err);
+              msg.success = "false";
+              msg.message = err.sqlMessage;
+              callback(msg);
+            }
+            completedQuery("View All Loan Records");
+          });
+          //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+          msg.books = rows;
+          //callback(msg);
         } else {
           console.log(err);
           msg.success = "false";
@@ -122,6 +175,69 @@ class LoanTDG {
         completedQuery("View All Loan Records");
       });
     });
+    //end all>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+    //get all the loaned Magazines   
+    // this.runQuery(function (conn, completedQuery) {
+    //   conn.query(sqlMagazine, (err, rows, fields) => {
+    //     if (!err) {
+    //       msg.success = "true";
+    //       msg.message = "no message";
+    //       msg.magazines = rows;
+    //       //callback(msg);
+    //     } else {
+    //       console.log(err);
+    //       msg.success = "false";
+    //       msg.message = err.sqlMessage;
+    //       callback(msg);
+    //     }
+    //     completedQuery("View All Loan Records");
+    //   });
+    // });
+    //end get magazines
+
+    //get all the loaned Music
+    
+    // this.runQuery(function (conn, completedQuery) {
+    //   conn.query(sqlMusic, (err, rows, fields) => {
+    //     if (!err) {
+    //       msg.success = "true";
+    //       msg.message = "no message";
+    //       msg.music = rows;
+    //       //callback(msg);
+    //     } else {
+    //       console.log(err);
+    //       msg.success = "false";
+    //       msg.message = err.sqlMessage;
+    //       callback(msg);
+    //     }
+    //     completedQuery("View All Loan Records");
+    //   });
+    // });
+    //end get Music
+
+    //get all the loaned Movies
+    
+    // this.runQuery(function (conn, completedQuery) {
+    //   conn.query(sqlMovies, (err, rows, fields) => {
+    //     if (!err) {
+    //       msg.success = "true";
+    //       msg.message = "no message";
+    //       msg.movies = rows;
+    //       //callback(msg);
+    //     } else {
+    //       console.log(err);
+    //       msg.success = "false";
+    //       msg.message = err.sqlMessage;
+    //       callback(msg);
+    //     }
+    //     completedQuery("View All Loan Records");
+    //   });
+    // });
+    //end get Movies
+
+    //callback(msg);
   }
 
   viewLoansForOneUser(userId, callback) {
